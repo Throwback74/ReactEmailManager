4a841fc41f1274943ac3aeadd10aeb8a2460a0da
Throwback74 <corey.slade@gmail.com>
2018-12-27 19:03:15 -0800

    update refactored code, adjusted folder structure, pending addition of mongoose

--- server/index.js
+++ server/index.js
@@ -1,26 +1,33 @@
 const express = require('express');
 const path = require('path');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
 require('./services/passport');
+
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Require returns a function which is immediately invoked with the app object
+require('./routes/authRoutes')(app);
+
 // Serve up static assets (usually on heroku)
 // Possibly don't need this
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
-
-
-// app.get('/', (req, res) => {
-//   res.send({ hi: 'there' });
-// });
 
 // Catch-all route for later once React side is setup - Sends every request to the React app
 // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, './client/build/index.html'));
 // });
 
 app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+  console.log(`🌎 ==> Server now live at http://localhost:${PORT}!`);
 });
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,10 +1,21 @@
-const passport = require("passport");
+const passport = require('passport');
 
-module.exports = () => {
+module.exports = app => {
   app.get(
-    "/auth/google",
-    passport.authenticate("google", { scope: ["profile", "email"] })
+    '/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
   );
 
-  app.get("/auth/google/callback", passport.authenticate("google"));
+  app.get('/auth/google/callback', passport.authenticate('google'));
 };
+
+
+// app.get('/', (req, res) => {
+//   res.send({ hi: 'there' });
+// });
+
+// Catch-all route for later once React side is setup - Sends every request to the React app
+// Define any API routes before this runs
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, './client/build/index.html'));
+// });
